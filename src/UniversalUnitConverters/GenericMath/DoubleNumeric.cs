2ac0514961b078c346a6ba5530de4dd71786d81f
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-06-14 00:03:18 +1200

    Adding generic math functionality with INumeric interface and its implementations for decimal, double and float.

---
+++ src/UniversalUnitConverters/GenericMath/DoubleNumeric.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversalUnitConverters.GenericMath
+{
+    /// <summary>
+    /// Implementation of INumeric for double type.
+    /// </summary>
+    /// <see cref="INumeric{T}"/>
+    public class DoubleNumeric : INumeric<double>
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b) => a / b;
+        public double Convert(decimal value) => (double)value;
+    }
+}
---
+++ src/UniversalUnitConverters/GenericMath/FloatNumeric.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversalUnitConverters.GenericMath
+{
+    /// <summary>
+    /// Implementation of INumeric for float type.
+    /// </summary>
+    /// <see cref="INumeric{T}"/>
+    public class FloatNumeric : INumeric<float>
+    {
+        public float Add(float a, float b) => a + b;
+        public float Subtract(float a, float b) => a - b;
+        public float Multiply(float a, float b) => a * b;
+        public float Divide(float a, float b) => a / b;
+        public float Convert(decimal value) => (float)value;
+    }
+}
